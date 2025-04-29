5e440b0b3f9fb11c1be97d480695037ae698c1b8
Felipe <fela.19silva@gmail.com>
2025-04-29 15:09:52 -0300

    refactor(storybook): remove dark theme configuration and base URL
    The dark theme configuration was removed from Storybook's manager and preview files to simplify the setup. Additionally, the base URL in the Vite config was removed as it is no longer needed for the project deployment.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,4 @@
 import type { Preview } from '@storybook/react';
-import { themes } from 'storybook/internal/theming';
 import '../src/index.css';
 const preview: Preview = {
 	parameters: {
@@ -8,9 +7,6 @@ const preview: Preview = {
 				color: /(background|color)$/i,
 				date: /Date$/i,
 			},
-		},
-		docs: {
-			theme: themes.dark,
 		},
 	},
 };
